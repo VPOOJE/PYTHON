b8d2c8c8853d10ea19953ccd592150dd9884b7b8
VPOOJE <130639089+VPOOJE@users.noreply.github.com>
2023-05-11 17:44:14 +0530

    basic prg1

---
+++ basic1.py
@@ -0,0 +1,7 @@
+Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x=20
+... y=10
+... u=(abs(x-y))
+... v=x+y
+... z=u*v
