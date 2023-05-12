7daf42c958bd15aecb75698f3f230058c7e6ffae
VPOOJE <130639089+VPOOJE@users.noreply.github.com>
2023-05-12 11:21:47 +0530

    selective3

---
+++ selective3.py
@@ -0,0 +1,9 @@
+num=int(input("enter the number:"))
+power=int(input("enter the power:"))
+if(power%2==0):
+    print("the nth power of a no is even:")
+else:
+    print("the nth power of a no is odd:")
+    
+    
+          
