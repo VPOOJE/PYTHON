38ed4d4c058e0ec313e528db55f3dcbeb6a4705c
VPOOJE <130639089+VPOOJE@users.noreply.github.com>
2023-05-14 11:02:55 +0530

    Add files via upload

--- basic2.py
+++ basic2.py
@@ -1,6 +1,6 @@
-Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> r=int(input("enter the radius:"))
-... h=int(input("enter the height:")
-... volume=3.14*r*r*h
-... print(volume)
+r=int(input("enter the radius:"))
+h=int(input("enter the height:"))
+volume=3.14*r*r*h
+surface_area=((2*3.14*r)*h) + ((3.14*r**2)*2)
+print(surface_area)
+print(volume)
