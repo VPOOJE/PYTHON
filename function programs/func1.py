9a51f76cec18c3263aa75c9d7c56d89fdeaf9f48
VPOOJE <130639089+VPOOJE@users.noreply.github.com>
2023-05-25 08:21:33 +0530

    func1.py

---
+++ function programs/func1.py
@@ -0,0 +1,6 @@
+'''Write a python code which checks whether the input number is a Prefect Square of not using a function.'''
+num=int(input("enter the number:"))
+if num**2:
+    print("perfect square")
+else:
+    print("not a perfect square")
