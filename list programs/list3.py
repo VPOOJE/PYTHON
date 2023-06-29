02f8fcf9bddf4a433b36f1bcc096ba4d080e90da
VPOOJE <130639089+VPOOJE@users.noreply.github.com>
2023-06-29 22:47:19 +0530

    Add files via upload

---
+++ list programs/list3.py
@@ -0,0 +1,17 @@
+'''Write a python program that reads integers from the user
+and stores them in a list. Your program should continue reading
+values until the user enters 0. Then it should display all of the
+values entered by the user (except for the 0) in ascending order
+(The list itself to be sorted).'''
+
+n = []
+while True:
+    num = int(input("Enter an integer (enter 0 to stop): "))
+    if num == 0:
+        break
+    n.append(num)
+print(n)
+n.sort()
+print("Sorted list of values:")
+for num in n:
+    print(num)
