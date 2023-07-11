98133a8c3d1cc985b2bbf5569dfce326990ad16a
VPOOJE <130639089+VPOOJE@users.noreply.github.com>
2023-07-11 10:53:43 +0530

    Delete func1.py

--- function programs/func1.py
+++
@@ -1,6 +0,0 @@
-'''Write a python code which checks whether the input number is a Prefect Square of not using a function.'''
-num=int(input("enter the number:"))
-if num**2:
-    print("perfect square")
-else:
-    print("not a perfect square")
